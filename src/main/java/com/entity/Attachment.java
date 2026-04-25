b7c45c07bee7264c0f5123543c6407cd1ae8390c
WEDSTERt <mrt.rov@yandex.ru>
2026-04-25 11:30:56 +0300

    ClearCode

--- src/main/java/com/entity/Attachment.java
+++ src/main/java/com/entity/Attachment.java
@@ -1,47 +1,5 @@
 package com.entity;
 
-import jakarta.persistence.*;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
-
-@Entity
-@Table(name = "attachments")
 public class Attachment {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "file_name", nullable = false)
-    private String fileName;
-
-    @Column(name = "file_type")
-    private String fileType;
-
-    @Column(name = "file_size")
-    private Long fileSize;
-
-    @Lob
-    @JdbcTypeCode(SqlTypes.VARBINARY)
-    @Column(name = "file_data", nullable = false, columnDefinition = "bytea")
-    private byte[] fileData;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "task_id", nullable = false)
-    private Task task;
-
-
-    // Геттеры и сеттеры
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getFileName() { return fileName; }
-    public void setFileName(String fileName) { this.fileName = fileName; }
-    public String getFileType() { return fileType; }
-    public void setFileType(String fileType) { this.fileType = fileType; }
-    public Long getFileSize() { return fileSize; }
-    public void setFileSize(Long fileSize) { this.fileSize = fileSize; }
-    public byte[] getFileData() { return fileData; }
-    public void setFileData(byte[] fileData) { this.fileData = fileData; }
-    public Task getTask() { return task; }
-    public void setTask(Task task) { this.task = task; }
 }
--- src/main/java/com/entity/User.java
+++ src/main/java/com/entity/User.java
@@ -1,9 +1,6 @@
 package com.entity;
 
 import jakarta.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Entity
 @Table(name = "users")
@@ -22,20 +19,6 @@ public class User {
     @Column(name = "user_password", nullable = false)
     private String userPassword;
 
-    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Project> ownedProjects = new ArrayList<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ProjectMember> projectMemberships = new ArrayList<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<SubgroupMember> subgroupMemberships = new ArrayList<>();
-
-    @OneToMany(mappedBy = "createdBy")
-    private List<Task> createdTasks = new ArrayList<>();
-
-    @ManyToMany(mappedBy = "assignees")
-    private List<Task> assignedTasks = new ArrayList<>();
 
     public User() {}
 
@@ -55,14 +38,4 @@ public class User {
     public String getUserPassword() { return userPassword; }
     public void setUserPassword(String userPassword) { this.userPassword = userPassword; }
 
-    public List<Project> getOwnedProjects() { return ownedProjects; }
-    public void setOwnedProjects(List<Project> ownedProjects) { this.ownedProjects = ownedProjects; }
-    public List<ProjectMember> getProjectMemberships() { return projectMemberships; }
-    public void setProjectMemberships(List<ProjectMember> projectMemberships) { this.projectMemberships = projectMemberships; }
-    public List<SubgroupMember> getSubgroupMemberships() { return subgroupMemberships; }
-    public void setSubgroupMemberships(List<SubgroupMember> subgroupMemberships) { this.subgroupMemberships = subgroupMemberships; }
-    public List<Task> getCreatedTasks() { return createdTasks; }
-    public void setCreatedTasks(List<Task> createdTasks) { this.createdTasks = createdTasks; }
-    public List<Task> getAssignedTasks() { return assignedTasks; }
-    public void setAssignedTasks(List<Task> assignedTasks) { this.assignedTasks = assignedTasks; }
 }
