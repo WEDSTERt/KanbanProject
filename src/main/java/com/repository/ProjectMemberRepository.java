15a2dd790b0652dc822cc3a7d7998256d9aacb16
reyrend <pervushiniljuha@yandex.ru>
2026-05-11 22:49:24 +0300

    back1

--- src/main/java/com/repository/ProjectMemberRepository.java
+++ src/main/java/com/repository/ProjectMemberRepository.java
@@ -1,8 +1,10 @@
 package com.repository;
 
+import com.entity.ProjectMember;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectMemberRepository{
-
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
+    boolean existsByProjectIdAndUserId(Long projectId, Long userId);
 }
