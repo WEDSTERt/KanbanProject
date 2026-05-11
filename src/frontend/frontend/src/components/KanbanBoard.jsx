59645fb3023ea449cadf93ebcba25af9af1711c4
WEDSTERt <mrt.rov@yandex.ru>
2026-05-11 17:52:11 +0300

    kanban_update

--- src/frontend/frontend/src/components/KanbanBoard.jsx
+++ src/frontend/frontend/src/components/KanbanBoard.jsx
@@ -1,3 +1,14 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useQuery, useMutation, useApolloClient } from '@apollo/client';
+import { GET_PROJECT_DETAILS } from '../graphql/queries';
+import { GET_TASKS_BY_SUBGROUP, GET_TASKS_BY_ASSIGNEE } from '../graphql/queries';
+import { UPDATE_TASK, DELETE_TASK, CREATE_TASK, SET_TASK_ASSIGNEES } from '../graphql/mutations';
+import { useAuth } from '../contexts/AuthContext';
+import SubgroupsPanel from './SubgroupsPanel';
+import TaskModal from './TaskModal';
+import ConfirmModal from './ConfirmModal';
+
 const KanbanBoard = () => {
     const navigate = useNavigate();
     const [searchParams, setSearchParams] = useSearchParams();
@@ -391,4 +402,5 @@ const KanbanBoard = () => {
         </div>
     );
 };
+
 export default KanbanBoard;
